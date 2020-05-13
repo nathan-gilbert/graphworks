5d010583a86bf4ef20e794a5d4433127000c28b1
Nathan Gilbert <nathan.gilbert@gmail.com>
2020-05-12 21:01:32 -0600

    Fix pylint, add some tests

--- tests/graph_tests.py
+++ tests/graph_tests.py
@@ -1,12 +1,18 @@
 import unittest
+import json
 
 from graphworks.graph import Graph
 
 
 class GraphTests(unittest.TestCase):
     def test_name(self):
-        g = Graph("graph")
-        self.assertEqual(g.name, 'graph')
+        graph = Graph("graph")
+        self.assertEqual(graph.name, 'graph')
+
+    def test_edges(self):
+        json_graph = {"name": "my graph", "edge": {"A": "B", "B": None}}
+        graph = Graph("graph", json.dumps(json_graph))
+        self.assertEqual(graph.edges, json_graph["edges"])
 
 
 if __name__ == '__main__':
