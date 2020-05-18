f4a2a7eb372a76c9648ebd4e65f8881dd285fa20
Nathan Gilbert <nathan.gilbert@gmail.com>
2020-05-18 17:57:01 -0600

    Added degree method and test

--- graphworks/algorithms/basic.py
+++ graphworks/algorithms/basic.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from graphworks.graph import Graph
 
 
@@ -17,7 +19,7 @@ def find_isolated_nodes(graph) -> list:
     return isolated
 
 
-def find_path(graph: Graph, start_vertex: str, end_vertex: str, path=None):
+def find_path(graph: Graph, start_vertex: str, end_vertex: str, path=None) -> List[str]:
     """ find a path from start_vertex to end_vertex in graph """
     if path is None:
         path = []
@@ -25,10 +27,38 @@ def find_path(graph: Graph, start_vertex: str, end_vertex: str, path=None):
     if start_vertex == end_vertex:
         return path
     if start_vertex not in graph:
-        return None
+        return []
     for vertex in graph[start_vertex]:
         if vertex not in path:
             extended_path = find_path(graph, vertex, end_vertex, path)
             if extended_path:
                 return extended_path
-    return None
+    return []
+
+
+def find_all_paths(graph: Graph, start_vertex: str, end_vertex: str, path=None) -> List[str]:
+    """ find all paths from start_vertex to end_vertex in graph """
+    if path is None:
+        path = []
+
+    path = path + [start_vertex]
+    if start_vertex == end_vertex:
+        return [path]
+    if start_vertex not in graph:
+        return []
+    paths = []
+    for vertex in graph[start_vertex]:
+        if vertex not in path:
+            extended_paths = find_all_paths(graph, vertex, end_vertex, path)
+            for ept in extended_paths:
+                paths.append(ept)
+    return paths
+
+
+def vertex_degree(graph, vertex):
+    """ The degree of a vertex is the number of edges connecting it,
+    i.e. the number of adjacent vertices. Loops are counted double,
+    i.e. every occurrence of a vertex in the list of adjacent vertices. """
+    adj_vertices = graph[vertex]
+    degree = len(adj_vertices) + adj_vertices.count(vertex)
+    return degree
--- tests/basic_tests.py
+++ tests/basic_tests.py
@@ -1,9 +1,11 @@
 import json
 import unittest
 
+from graphworks.algorithms.basic import find_all_paths
 from graphworks.algorithms.basic import find_isolated_nodes
 from graphworks.algorithms.basic import find_path
 from graphworks.algorithms.basic import generate_edges
+from graphworks.algorithms.basic import vertex_degree
 from graphworks.graph import Graph
 
 
@@ -37,11 +39,34 @@ class BasicTests(unittest.TestCase):
         path = find_path(graph, "a", "b")
         self.assertListEqual(['a', 'd', 'c', 'b'], path)
         path = find_path(graph, "a", "f")
-        self.assertIsNone(path)
+        self.assertListEqual([], path)
         path = find_path(graph, "c", "c")
         self.assertListEqual(['c'], path)
         path = find_path(graph, "z", "a")
-        self.assertIsNone(path)
+        self.assertListEqual([], path)
+
+    def test_find_all_paths(self):
+        json_graph = {"label": "test2",
+                      "directed": False,
+                      "graph":
+                          {"a": ["d", "f"],
+                           "b": ["c"],
+                           "c": ["b", "c", "d", "e"],
+                           "d": ["a", "c"],
+                           "e": ["c"],
+                           "f": ["d"]}
+                      }
+        graph = Graph(input_graph=json.dumps(json_graph))
+        paths = find_all_paths(graph, "a", "b")
+        self.assertListEqual([['a', 'd', 'c', 'b'], ['a', 'f', 'd', 'c', 'b']], paths)
+        paths = find_all_paths(graph, "z", "b")
+        self.assertListEqual([], paths)
+
+    def test_vertex_degree(self):
+        json_graph = {"graph": {"a": ["a", "b", "c"], "b": ["a"], "c": ["a"]}}
+        graph = Graph(input_graph=json.dumps(json_graph))
+        deg = vertex_degree(graph, 'a')
+        self.assertEqual(4, deg)
 
 
 if __name__ == '__main__':
